4aaa5beabf1cae50fe49771d7ab435157e856cda
chihokyo <chihoukyo@gmail.com>
2021-04-07 16:59:51 +0900

    【算法课程】线性查找-线性查找测试

--- course/First/LinearSearch/src/LinearSearch.java
+++ course/First/LinearSearch/src/LinearSearch.java
@@ -4,18 +4,33 @@ public class LinearSearch {
     }
 
     public static void main(String[] args) {
+        // 写一个需要的测试数据量
+        int[] loopSize = {10_000_000, 1_00_000_000};
+        // 循环上面的数据量
+        for (int n : loopSize) {
+            // 生成一个列表
+            Integer[] data = ArrayGenerator.generateOrderedArray(n);
+            long startTime = System.nanoTime();
+            for (int j = 0; j < 100; j++) {
+                int result = LinearSearch.search(data, n);
+            }
+            long endTime = System.nanoTime();
+            double time = (endTime - startTime) / 1000000000.0;
+            System.out.println("数据规模: " + n + ",运行速度: " + time + "s");
 
-        Integer[] data = {100, 16 - 9, 0, 1, 13, 18};
-        int res1 = LinearSearch.search(data, 18); // 自动解包 包装类
-        int res2 = LinearSearch.search(data, 1000); // Java类型推断可以省略方法前面的类型 LinearSearch.<Integer>search
-        System.out.println(res1);
-        System.out.println(res2);
+        }
 
-        System.out.println("****自定义Student类测试***");
-        Student[] students = {new Student("Amy"), new Student("Kitty"), new Student("Tom")};
-        Student kitty = new Student("Kitty");
-        int res3 = LinearSearch.search(students, kitty);
-        System.out.println(res3);
+
+//        int res1 = LinearSearch.search(data, 11); // 自动解包 包装类
+//        int res2 = LinearSearch.search(data, 1000); // Java类型推断可以省略方法前面的类型 LinearSearch.<Integer>search
+//        System.out.println(res1);
+//        System.out.println(res2);
+//
+//        System.out.println("****自定义Student类测试***");
+//        Student[] students = {new Student("Amy"), new Student("Kitty"), new Student("Tom")};
+//        Student kitty = new Student("Kitty");
+//        int res3 = LinearSearch.search(students, kitty);
+//        System.out.println(res3);
 
     }
 
